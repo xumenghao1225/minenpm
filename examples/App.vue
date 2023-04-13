50d9a1f1c2c888d39d8cf7ea8daa2d108bc848d5
xumenghao <xumenghao1225@163.com>
2023-04-13 12:47:13 +0800

    fix: fsdf

--- examples/App.vue
+++ examples/App.vue
@@ -6,7 +6,7 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 @Component({
-	name: "Ap0.0.0.p",
+	name: "App",
 })
 export default class App extends Vue {
 	name = "App";
